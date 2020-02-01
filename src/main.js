f24d3e6a501b038f4eaa00ae1e1841a9dfacc2c8
onlyess <django_lf@163.com>
2020-02-01 16:07:49 +0800

    router
    Change-Id: I4c7d3139066e696544010b144e2b30adcad40191

--- src/main.js
+++ src/main.js
@@ -6,19 +6,6 @@ import './plugins/element.js';
 
 Vue.config.productionTip = false
 
-// //路由权限
-// router.beforeEach((to, from, next) => {
-//   let isLogin = cookies.get('info');
-//   if (!isLogin) {
-//     //未登录
-//     if (to.path !== '/login') {
-//       //跳转到登录页
-//       return next({ path: '/login' });
-//     } else {
-//       next();
-//     }
-//   }
-// });
 
 new Vue({
   router,
