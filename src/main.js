eda31b55d0e5924b479166f318fcfde997661d02
onlyess <django_lf@163.com>
2020-02-01 16:08:20 +0800

    router
    Change-Id: If69a7f97b5e54b2d685aaa739366bc957a4bf149

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
-// import cookies from 'vue-cookies'
 import './plugins/element.js';
 
 Vue.config.productionTip = false
